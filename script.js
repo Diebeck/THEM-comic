b6bd9e8b6801c73ad6f5ffce8841f9ac6447d487
Diebeck <romeoreyalonso@gmail.com>
2026-09-06 13:21:05 +0200

    Added mobile support and nice Firefox scrollbar

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const meta = document.createElement("meta")
+meta.name = "viewport"
+meta.content = "width=device-width, initial-scale=1"
+document.head.appendChild(meta)
+
 const link = document.createElement("link")
 link.rel = "stylesheet"
 link.href = "../../style.css"
